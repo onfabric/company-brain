8cc8e12e1656577647600528805ca6580995bf01
massimoalbarello <massimo00albarello@gmail.com>
2026-05-29 15:30:36 +0100

    refactor(slack): remove legacy syncs

--- nango-integrations/index.ts
+++ nango-integrations/index.ts
@@ -1,5 +1,1 @@
-import './slack/syncs/channels.js';
-import './slack/syncs/conversations.js';
-import './slack/syncs/messages-received.js';
 import './slack/syncs/threads.js';
-import './slack/syncs/users.js';
